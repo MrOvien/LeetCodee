055b5b119b3c696377722c9de95ae5643b759a26
MrOvien <TruToz64@gmail.com>
2023-10-09 16:31:09 +0100

    excel comuln title

---
+++ columnTitle.py
@@ -0,0 +1,14 @@
+columnTitle = "FXSHRXW"
+if len(columnTitle) == 1:
+    print(ord(columnTitle) - 64)
+
+lists = list(columnTitle)
+# print(lists)
+math=0
+for i in range(0 , len(lists)):
+
+    math += 26**i * (ord(lists[len(lists) - i - 1]) - 64)
+    # math2 = (ord(lists[len(lists) - i - 1]))
+    # print(lists[i])
+    # print(math, math2)
+print(math)
---
+++ main.py
@@ -0,0 +1,3 @@
+columnTitle = "A"
+if len(columnTitle) == 1:
+    print(ord(columnTitle) - 64)
