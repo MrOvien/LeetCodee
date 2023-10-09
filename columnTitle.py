3802eb5ee982504eec96ac6ebe56b64d6ece9e54
MrOvien <TruToz64@gmail.com>
2023-10-09 16:32:47 +0100

    Update columnTitle.py

--- columnTitle.py
+++ columnTitle.py
@@ -10,5 +10,5 @@ for i in range(0 , len(lists)):
     math += 26**i * (ord(lists[len(lists) - i - 1]) - 64)
     # math2 = (ord(lists[len(lists) - i - 1]))
     # print(lists[i])
-    # print(math, math2)2
+    # print(math, math2)
 print(math)
